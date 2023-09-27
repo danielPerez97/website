8c1e891361bdd240c0d47d6c7aac23d7669817c8
Daniel Perez <doperez1997@gmail.com>
2023-09-27 01:22:46 -0600

    Initial draft for PragmaticDaggerArticle2.md

--- src/components/blog/loaders/loadBlogPosts.jsx
+++ src/components/blog/loaders/loadBlogPosts.jsx
@@ -1,7 +1,9 @@
 import DaggerByExample from '../../../posts/PragmaticDaggerArticle1.md';
+import PragmaticDaggerArticle2 from '../../../posts/PragmaticDaggerArticle2.md';
 
 export async function loadBlogPosts() {
   const daggerByExampleText = (await (await fetch(DaggerByExample)).text());
+  const pragmaticDaggerArticle2 = (await (await fetch(PragmaticDaggerArticle2)).text());
   // eslint-disable-next-line no-console
   const blogs = [
     {
@@ -10,6 +12,13 @@ export async function loadBlogPosts() {
       shortDescription: 'Learning to use Dagger for the first time, thermosiphon and car engine free.',
       publishDate: '9/19/2023. Special thanks to Jesse Wilson for review.',
       body: daggerByExampleText,
+    },
+    {
+      id: 2,
+      title: 'A Pragmatic Introduction To Dagger on Android Part 2: Retrofit Services',
+      shortDescription: 'Learn how to up Dagger 2 in an actual Android project.',
+      publishDate: 'TBA',
+      body: pragmaticDaggerArticle2,
     },
   ];
 
